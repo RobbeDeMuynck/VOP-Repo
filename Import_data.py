e9a0e13ef4553495504b6f0f4c038d7912a6e7e6
robbedb36 <100784250+robbedb36@users.noreply.github.com>
2022-03-09 16:29:22 +0100

    update 1.01

--- Import_data.py
+++ Import_data.py
@@ -8,13 +8,6 @@ import torch.nn.functional as F
 import torch.optim as optim
 import imageio
 
-train = Import_data("C:\\Users\\Werk\\OneDrive - UGent\\Burgie 4\\Sem 2\\VOP\\Coding\\microCT data X-Cube\\584_l0r0\\20211021150245_CT_ISRA_0.dcm")
-test = Import_data("C:\\Users\\Werk\\OneDrive - UGent\\Burgie 4\\Sem 2\\VOP\\Coding\\microCT data X-Cube\\584_l0r0\\20211021150245_CT_ISRA_0.dcm")
-
-#load and shuffle data
-trainset = torch.utils.data.DataLoader(train,batch_size=10,shuffle=True)
-testset = torch.utils.data.DataLoader(test,batch_size=10,shuffle=False)
-
 class Import_data():
   def __init__(self, filename_train, filename_test):
     self.train, self.test = None, None
@@ -23,8 +16,10 @@ class Import_data():
     return np.exp(x)/np.sum(np.exp(x))
 
   def normalize(self):
-    self.test = (self.test-np.mean(self.test))/np.std(self.test)
-    self.train = (self.train-np.mean(self.train))/np.std(self.train)
+    for i in range(len(self.test)):
+      self.test[i] = (self.test[i]-np.mean(self.test[i]))/np.std(self.test[i])
+    for i in range(len(self.train)):
+      self.train[i] = (self.train[i]-np.mean(self.train[i]))/np.std(self.train[i])
     return None
   
   def maxpool(self,x):
@@ -41,13 +36,15 @@ class Import_data():
     list_traindata = []
     for i in range(len(self.train)):
       list_traindata.append(self.train[4*i:4*i+4])
-    return list_traindata
+    self.train = list_traindata
+    return None
 
   def arrange_testdata(self):
     list_testdata = []
     for i in range(len(self.test)):
       list_testdata.append(self.test[4*i:4*i+4])
-    return list_testdata
+    self.test = list_testdata
+    return None
 
   def load_data(self,filename_train, filename_test):
     imageio.plugins.freeimage.download()
@@ -55,8 +52,8 @@ class Import_data():
     return None
   
   def shape_data(self):
-    
-
+    self.train = self.train.view[-1,40000]
+    self.test = self.test.view[-1,40000]
     return None
 
   def run(self):
@@ -65,3 +62,11 @@ class Import_data():
     self.arrange_testdata()
     self.arrange_traindata()
     self.shape_data()
+
+
+train = Import_data("C:\\Users\\Werk\\OneDrive - UGent\\Burgie 4\\Sem 2\\VOP\\Coding\\microCT data X-Cube\\584_l0r0\\20211021150245_CT_ISRA_0.dcm")
+test = Import_data("C:\\Users\\Werk\\OneDrive - UGent\\Burgie 4\\Sem 2\\VOP\\Coding\\microCT data X-Cube\\584_l0r0\\20211021150245_CT_ISRA_0.dcm")
+
+#load and shuffle data
+trainset = torch.utils.data.DataLoader(train,batch_size=10,shuffle=True)
+testset = torch.utils.data.DataLoader(test,batch_size=10,shuffle=False)
