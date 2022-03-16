e244f403ae2adaf17c97bdfb91000bae1e4bae73
Thibault <thibault.sys@hotmail.com>
2022-03-16 14:31:26 +0100

    data

--- Read_Processed_Data.py
+++ Read_Processed_Data.py
@@ -37,15 +37,16 @@ axs[0].imshow(d["M08_-001h"][80],cmap = 'bone')
 axs[1].imshow(d["M08_0.25h"][80],cmap = 'bone')
 axs[2].imshow(d["M08_024h"][80],cmap = 'bone')
 
-#plt.show()
+plt.show()
+#print((d["M08_-001h"][80]))
 
 #Testje Organ --> onduidelijk nog
 
 # Plots
-fig, axs = plt.subplots(1,3, figsize=(10,10), sharex=True)
-axs[0].imshow(o["M08_-001h_Organ"][110],cmap = 'bone')
-axs[1].imshow(o["M08_0.25h_Organ"][110],cmap = 'bone')
-axs[2].imshow(o["M08_024h_Organ"][110],cmap = 'bone')
+#fig, axs = plt.subplots(1,3, figsize=(10,10), sharex=True)
+#axs[0].imshow(o["M08_-001h_Organ"][110],cmap = 'bone')
+#axs[1].imshow(o["M08_0.25h_Organ"][110],cmap = 'bone')
+#axs[2].imshow(o["M08_024h_Organ"][110],cmap = 'bone')
 
 
-plt.show()
+#plt.show()
---
+++ TrainData.py
@@ -0,0 +1,26 @@
+import nibabel as nib
+import numpy as np
+import pathlib
+import matplotlib.pyplot as plt
+#print('check 12')
+
+Train_voor = []
+Train_na = []
+
+path = pathlib.Path(__file__).parent
+for mouse in ["M03", "M04", "M05", "M06", "M07", "M08"]:
+    for timestamp in ["-001h", "024h"]:
+        if timestamp == "-001h":
+            path_ct = path / f"processed/{mouse}_{timestamp}_CT280.img"
+            Train_voor.append(nib.load(path_ct).get_fdata())
+        else:
+            path_ct = path / f"processed/{mouse}_{timestamp}_CT280.img"
+            Train_na.append(nib.load(path_ct).get_fdata())
+
+
+# Plots
+fig, axs = plt.subplots(1,2, figsize=(10,10), sharex=True)
+axs[0].imshow(Train_voor[5][10],cmap = 'bone')
+axs[1].imshow(Train_na[5][10],cmap = 'bone')
+
+plt.show()
