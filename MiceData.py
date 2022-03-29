9122e3f07147d2be8a16cfe9d600cd7edfbe2522
RobbeDeMuynck <robbe_demuynck@yahoo.com>
2022-03-29 21:35:47 +0200

    TEST

--- MiceData.py
+++ MiceData.py
@@ -1,7 +1,7 @@
 import pathlib
 import nibabel as nib
 
-##################################### LOADING DATA  ##################################
+##################################### LOADING DATA TRANSVERSAL  ##################################
 Train_voor = []
 Train_na = []
 
@@ -45,3 +45,49 @@ for timestamp in ["-001h", "024h"]:
 # Train_transversal_024h (1210 slices)
 # Test_transversal_001h  (242 slices)
 # Test_transversal_024h  (242 slices)
+
+##################################### LOADING DATA SAGITTAL ##################################
+Train_voor = []
+Train_na = []
+
+path = pathlib.Path(__file__).parent
+for timestamp in ["-001h", "024h"]:
+    for mouse in ["M03", "M04", "M05", "M06", "M07"]:
+        if timestamp == "-001h":
+            path_ct = path / f"processed/{mouse}_{timestamp}_CT280.img"
+            Train_voor.append(nib.load(path_ct).get_fdata())
+        else: 
+            path_ct = path / f"processed/{mouse}_{timestamp}_CT280.img"
+            Train_na.append(nib.load(path_ct).get_fdata())           
+
+
+# nuttige slices lopen van idx 25 tot 125
+Train_sagittal_001h = []
+Train_sagittal_024h = []
+for mouse in Train_voor:
+    for slice in mouse[25:125]:
+        Train_sagittal_001h.append(slice)
+
+for mouse in Train_na:
+    for slice in mouse[25:125]:
+        Train_sagittal_024h.append(slice)
+
+
+Test_sagittal_001h = []
+Test_sagittal_024h = []
+
+for timestamp in ["-001h", "024h"]:
+    mouse = "M08"
+    path_ct = path / f"processed/{mouse}_{timestamp}_CT280.img"
+    ct = nib.load(path_ct).get_fdata()
+    for slice in ct[25:125]:
+        if timestamp == "-001h":
+            Test_sagittal_001h.append(slice)
+        else:
+            Test_sagittal_024h.append(slice)
+
+
+#Train_sagittal_001h (500 slices)
+#Train_sagittal_024h (500 slices)
+#Test_sagittal_001h  (100 slices)
+#Test_sagittal_024h  (100 slices)
--- test.py
+++ test.py
@@ -8,11 +8,13 @@ num_epochs = params.num_epochs
 batch_size = params.batch_size
 learning_rate = params.learning_rate
 weight_decay = params.weight_decay
-plotting = False
+plotting = True
 input = MiceData.Test_transversal_001h
 target = MiceData.Test_transversal_024h
 # input = MiceData.Train_transversal_001h
 # target = MiceData.Train_transversal_024h
+# input = MiceData.Test_sagittal_001h
+# target = MiceData.Test_sagittal_024h
 
 ############################# IMPORTING THE NEEDED FUNCTIONS  #############################
 
@@ -51,21 +53,21 @@ for i, (input_batch,target_batch) in enumerate(tqdm(test_loader)):
     loss = loss_function(prediction_batch,target_batch) #vergelijk predicted na image met de echte na image
     losses.append(loss.item())
 
-    if plotting == True and i%50 == 0:
+    if plotting == True and i%80 == 0:
         for j in range(batch_size):
             fig = plt.subplots(figsize=(20,40))
             img_pred = prediction_batch[j][0].cpu()
             img_input = input_batch[j][0].cpu()
             img_target = target_batch[j][0].cpu()
             plt.subplot(1,3,1)
-            plt.imshow(img_input.detach().cpu().numpy(),cmap='bone')
+            plt.imshow(img_input.detach().cpu().numpy(),cmap='viridis')
             plt.title('Before injection')
             
             plt.subplot(1,3,2)
-            plt.imshow(img_target.detach().numpy(),cmap='bone')
+            plt.imshow(img_target.detach().numpy(),cmap='viridis')
             plt.title('24 hours after injection')
             plt.subplot(1,3,3)
-            plt.imshow(img_pred.detach().numpy(),cmap='bone')
+            plt.imshow(img_pred.detach().numpy(),cmap='viridis')
             plt.title('Model prediction')
             plt.show()
 
