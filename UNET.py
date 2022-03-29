963569d8074b18666e15c8ab4ef4b9c5ca3c6555
RobbeDeMuynck <robbe_demuynck@yahoo.com>
2022-03-29 15:41:25 +0200

    Code adjust

--- UNET.py
+++ UNET.py
@@ -18,55 +18,6 @@ import params
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 torch.cuda.empty_cache()
 
-##################################### LOADING DATA  ##################################
-
-Train_voor = []
-Train_na = []
-
-path = pathlib.Path('processed').parent
-for timestamp in ["-001h", "024h"]:
-     for mouse in ["M03", "M04", "M05", "M06", "M07"]:
-         if timestamp == "-001h":
-             path_ct = path / f"processed/{mouse}_{timestamp}_CT280.img"
-             Train_voor.append(nib.load(path_ct).get_fdata())
-         else: 
-             path_ct = path / f"processed/{mouse}_{timestamp}_CT280.img"
-             Train_na.append(nib.load(path_ct).get_fdata())           
-
-
-Train_Data_001h = []
-Train_Data_024h = []
-
-for mouse in Train_voor:
-    for i in range(mouse.shape[-1]):
-        Train_Data_001h.append(mouse[:,:,i])
-
-
-for mouse in Train_na:
-    for i in range(mouse.shape[-1]):
-         Train_Data_024h.append(mouse[:,:,i])
-
-
-Test_Data_001h = []
-Test_Data_024h = []
-
-for timestamp in ["-001h", "024h"]:
-     mouse = "M08"
-     path_ct = path / f"processed/{mouse}_{timestamp}_CT280.img"
-     ct = nib.load(path_ct).get_fdata()
-     for i in range(ct.shape[-1]):
-        if timestamp == "-001h":
-            Test_Data_001h.append(ct[:,:,i])
-        else:
-            Test_Data_024h.append(ct[:,:,i])
-
-#Train_Data_001h (1210 slices)
-#Train_Data_024h (1210 slices)
-#Test_Data_001h  (242 slices)
-#Test_Data_024h  (242 slices)
-
-
-
 
 ##################################### DEFINING BUILDING BLOCKS  ##################################
 
