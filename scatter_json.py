9465b5343ab9a19f4d6fbc75768e5c24a02b2f60
RobbeDeMuynck <robbe_demuynck@yahoo.com>
2022-03-30 10:35:31 +0200

    Processing of runlogs

--- scatter_json.py
+++ scatter_json.py
@@ -6,19 +6,24 @@ import numpy as np
 sns.set_theme(style="white")
 import matplotlib.pyplot as plt
 import json
+from pathlib import Path
 
-with open("runlog.json", "r") as file:
-    data = json.load(file)
+
+
 
 cols = ["Batch size","Learning rate","Weight decay","Starting features","Minimum validation loss","Epochs till convergence"]
 vals = {col: [] for col in cols}
-for run in data.values():
-    vals["Batch size"].append(run["batch_size"])
-    vals["Learning rate"].append(run["learning_rate"])
-    vals["Weight decay"].append(["weight_decay"])
-    vals['Starting features'].append(run["features"])
-    vals["Minimum validation loss"].append(min(run["val_loss"]))
-    vals["Epochs till convergence"].append(run["num_epoch_convergence"])
+files = Path('runlogs').glob('*')
+for file in files:
+    with open(file, "r") as RUN:
+        run = json.load(RUN)
+        vals["Batch size"].append(run["batch_size"])
+        vals["Learning rate"].append(run["learning_rate"])
+        vals["Weight decay"].append(["weight_decay"])
+        vals['Starting features'].append(run["features"])
+        vals["Minimum validation loss"].append(min(run["val_loss"]))
+        vals["Epochs till convergence"].append(run["num_epoch_convergence"])
+        # print('RUN')
 
 Data = pd.DataFrame(data=vals,columns=cols)
 print(Data.head())
