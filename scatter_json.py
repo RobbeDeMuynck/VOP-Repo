7965dfe55ca1dd3c11c6b7a3db8b4db1952cf08f
RobbeDeMuynck <robbe_demuynck@yahoo.com>
2022-03-30 18:29:59 +0200

    VOPPEN!

--- scatter_json.py
+++ scatter_json.py
@@ -10,50 +10,50 @@ from pathlib import Path
 
 files = Path('runlogs').glob('*')
 
-# cols = ["Batch size","Learning rate","Weight decay","Starting features","Minimum validation loss","Epochs till convergence"]
-# vals = {col: [] for col in cols}
-# for file in files:
-#     with open(file, "r") as RUN:
-#         run = json.load(RUN)
-#         vals["Batch size"].append(run["batch_size"])
-#         vals["Learning rate"].append(run["learning_rate"])
-#         vals["Weight decay"].append(["weight_decay"])
-#         vals['Starting features'].append(run["features"])
-#         vals["Minimum validation loss"].append(min(run["val_loss"]))
-#         vals["Epochs till convergence"].append(run["num_epoch_convergence"])
-
-# Data = pd.DataFrame(data=vals,columns=cols)
-# print(Data.head())
-# sns.relplot(data=Data, x="Epochs till convergence", y="Minimum validation loss", 
-#             hue="Learning rate", style = "Starting features", size="Batch size",
-#             sizes=(50, 350), alpha=.5, palette="crest", height=6)
-# plt.show()
-
-losses = []
-PAR = [(8, 0.001, 0.09, 4)]
+cols = ["Batch size","Learning rate","Weight decay","Starting features","Minimum validation loss","Epochs trained"]
+vals = {col: [] for col in cols}
 for file in files:
     with open(file, "r") as RUN:
         run = json.load(RUN)
-        params = run["batch_size"], run["learning_rate"], run["weight_decay"], run["features"]
-        train_loss = run["train_loss"]
-        val_loss = run["val_loss"]
-        if params in PAR:
-            losses = train_loss, val_loss
-            print(run["num_epoch_convergence"])
+        vals["Batch size"].append(run["batch_size"])
+        vals["Learning rate"].append(run["learning_rate"])
+        vals["Weight decay"].append(["weight_decay"])
+        vals['Starting features'].append(run["features"])
+        vals["Minimum validation loss"].append(min(run["val_loss"]))
+        vals["Epochs trained"].append(run["num_epoch_convergence"])
 
-n = len(losses)
-palette = sns.color_palette("mako_r", n)
-palette = sns.color_palette("crest", n)
-fig, ax = plt.subplots(1, 1)
-ax.semilogy(losses[0], color=palette[0], label='Training losses')
-ax.semilogy(losses[1], color=palette[1], label='Validation losses')
-ax.axvline(x=np.argmin(losses[1]), c='k', ls='--', label='Last saved version')
-#f'BS=%d, LR=%.3f, WD=%.2f, FT=%d'%tuple(PAR[0])
-ax.set_title('MSE loss decay per epoch')
-ax.set_xlabel('Epoch number')
-ax.set_ylabel('Training losses')
-ax.legend()
-ax.grid()
+Data = pd.DataFrame(data=vals,columns=cols)
+print(Data.head())
+sns.relplot(data=Data, x="Epochs trained", y="Minimum validation loss", 
+            hue="Learning rate", style = "Starting features", size="Batch size",
+            sizes=(50, 350), alpha=.5, palette="crest", height=6)
 plt.show()
+
+# losses = []
+# PAR = [(8, 0.001, 0.09, 4)]
+# for file in files:
+#     with open(file, "r") as RUN:
+#         run = json.load(RUN)
+#         params = run["batch_size"], run["learning_rate"], run["weight_decay"], run["features"]
+#         train_loss = run["train_loss"]
+#         val_loss = run["val_loss"]
+#         if params in PAR:
+#             losses = train_loss, val_loss
+#             print(run["num_epoch_convergence"])
+#
+# n = len(losses)
+# palette = sns.color_palette("mako_r", n)
+# palette = sns.color_palette("crest", n)
+# fig, ax = plt.subplots(1, 1)
+# ax.semilogy(losses[0], color=palette[0], label='Training losses')
+# ax.semilogy(losses[1], color=palette[1], label='Validation losses')
+# ax.axvline(x=np.argmin(losses[1]), c='k', ls='--', label='Last saved version')
+# #f'BS=%d, LR=%.3f, WD=%.2f, FT=%d'%tuple(PAR[0])
+# ax.set_title('MSE loss decay per epoch')
+# ax.set_xlabel('Epoch number')
+# ax.set_ylabel('MSE loss')
+# ax.legend()
+# ax.grid()
+# plt.show()
 
 # sns.lineplot(data=Data, y="Training", palette=palette)
--- test.py
+++ test.py
@@ -12,17 +12,17 @@ patience = params.patience
 features = params.features
 
 plotting = True
-input = MiceData.Test_transversal_001h
-target = MiceData.Test_transversal_024h
+# input = MiceData.Test_transversal_001h
+# target = MiceData.Test_transversal_024h
 # input = MiceData.Train_transversal_001h
 # target = MiceData.Train_transversal_024h
-# input = MiceData.Test_sagittal_001h
-# target = MiceData.Test_sagittal_024h
+input = MiceData.Test_sagittal_001h
+target = MiceData.Test_sagittal_024h
 
 ############################# IMPORTING THE NEEDED FUNCTIONS  #############################
 
 ############################# LOADING THE MODEL  #############################
-model_path = "model_test.pth"
+model_path = "MODELS/BS=8;LR=0.001;WD=0.09;FT=4.pth"
 model = UNet(features).to(device)
 model.load_state_dict(torch.load(model_path))
 
@@ -56,22 +56,37 @@ for i, (input_batch,target_batch) in enumerate(tqdm(test_loader)):
     loss = loss_function(prediction_batch,target_batch) #vergelijk predicted na image met de echte na image
     losses.append(loss.item())
 
-    if plotting == True and i%80 == 0:
-        for j in range(batch_size):
-            fig = plt.subplots(figsize=(20,40))
-            img_pred = prediction_batch[j][0].cpu()
-            img_input = input_batch[j][0].cpu()
-            img_target = target_batch[j][0].cpu()
-            plt.subplot(1,3,1)
-            plt.imshow(img_input.detach().cpu().numpy(),cmap='viridis')
-            plt.title('Before injection')
+    # if plotting == True and i%80 == 0:
+    #     for j in range(batch_size):
+    #         fig = plt.subplots(figsize=(20,40))
+    #         img_pred = prediction_batch[j][0].cpu()
+    #         img_input = input_batch[j][0].cpu()
+    #         img_target = target_batch[j][0].cpu()
+    #         plt.subplot(1,3,1)
+    #         plt.imshow(img_input.detach().cpu().numpy(),cmap='viridis')
+    #         plt.title('Before injection')
             
-            plt.subplot(1,3,2)
-            plt.imshow(img_target.detach().numpy(),cmap='viridis')
-            plt.title('24 hours after injection')
-            plt.subplot(1,3,3)
-            plt.imshow(img_pred.detach().numpy(),cmap='viridis')
-            plt.title('Model prediction')
-            plt.show()
-
+    #         plt.subplot(1,3,2)
+    #         plt.imshow(img_target.detach().numpy(),cmap='viridis')
+    #         plt.title('24 hours after injection with contrast enhancement')
+    #         plt.subplot(1,3,3)
+    #         plt.imshow(img_pred.detach().numpy(),cmap='viridis')
+    #         plt.title('Model prediction')
+    #         plt.show()
+    if plotting == True and i%80 == 0:
+         for j in range(batch_size):
+             fig = plt.subplots(figsize=(20,40))
+             img_pred = prediction_batch[j][0].cpu()
+             img_input = input_batch[j][0].cpu()
+             img_target = target_batch[j][0].cpu()
+             plt.subplot(3,1,1)
+             plt.imshow(img_input.detach().cpu().numpy(),cmap='viridis')
+             plt.title('Before injection')
+             plt.subplot(3,1,2)
+             plt.imshow(img_target.detach().numpy(),cmap='viridis')
+             plt.title('24 hours after injection with contrast enhancement')
+             plt.subplot(3,1,3)
+             plt.imshow(img_pred.detach().numpy(),cmap='viridis')
+             plt.title('Model prediction')
+             plt.show()
 print(f'The average test loss is: {np.mean(np.array(losses))}')
