3fa090266ec848ed6a9523f623d8b26e45eda5bf
RobbeDeMuynck <robbe_demuynck@yahoo.com>
2022-04-15 16:11:13 +0200

    layers added

--- _UNET.py
+++ _UNET.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn 
 import torchvision
-import numpy as np
-from torch.utils.data import Dataset
 
 # import torch
 # import numpy as np
@@ -152,33 +150,3 @@ class UNet(nn.Module):
         ### Last layer: mapping to prediction ###
         outputs = self.outputs(up)
         return outputs
-
-################################## INITIALIZING THE DATALOADER  ##################################
-
-class MiceDataset(Dataset):
-    def __init__(self, data_voor, data_na, p=0.5):
-        super().__init__()
-        self.data_voor = (data_voor - np.mean(data_voor))/np.std(data_voor) #vanwege de kleine dataset laden we het gewoon helemaal in memory en normaliseren we in place
-        self.data_na = (data_na - np.mean(data_na))-np.std(data_na)
-        self.p = p
-
-    def __len__(self):
-        return len(self.data_voor)
-
-    def __getitem__(self, index):
-
-        input = self.data_voor[index]
-        target = self.data_na[index]
-        
-        ###  DATA AUGMENTATION
-        if torch.rand(1) < self.p:
-            input = np.flipud(input)
-            target = np.flipud(target)
-
-        if torch.rand(1) < self.p:
-            input = np.fliplr(input)
-            target = np.fliplr(target)
-        
-        input = torch.from_numpy(input.copy()).unsqueeze(0).float()
-        target = torch.from_numpy(target.copy()).unsqueeze(0).float()
-        return input, target
