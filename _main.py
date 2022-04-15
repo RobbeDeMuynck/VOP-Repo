76a262ed04fc0827b3c6b9c87bd2a57c145586e0
RobbeDeMuynck <robbe_demuynck@yahoo.com>
2022-04-16 01:53:11 +0200

    layers added, testruns done

--- _main.py
+++ _main.py
@@ -9,22 +9,28 @@ torch.cuda.empty_cache()
 
 ### Declare training hyperparameters ###
 num_epochs = 120
-batch_size = 4
-learning_rate = 1e-3
-weight_decay = 0.9
+batch_size = [4]
+learning_rate = [1e-4]
+weight_decay = [0]
 patience = 5
 
 ### Declare training & validation datasets ###
-input, target, val_input, val_target = get_data(plane='sagittal', val_mouse=0)
+input, target, val_input, val_target = get_data(plane='transversal', val_mouse=0)
 train_loader = DataLoader(MiceDataset(input, target), batch_size=batch_size, shuffle=True, drop_last=True)
 val_loader = DataLoader(MiceDataset(val_input, val_target), batch_size=batch_size, shuffle=True, drop_last=True)
 
 ### Declare network architecture ###
-features = 8
-layers = 4
+features = [10]
+layers = [3]
 
 ### Train model ###
-train(layers, features, device,
-        train_loader, val_loader,
-        num_epochs, batch_size, learning_rate=learning_rate, weight_decay=weight_decay, patience=5,
-        model_name='TEST2', save=True)
+for FT in features:
+        for LYRS in layers:
+                for BS in batch_size:
+                        for LR in learning_rate:
+                                for WD in weight_decay:
+                                        model_name = f'LYRS={LYRS};FT={FT};BS={BS};LR={LR};WD={WD}'
+                                        train(LYRS, FT, device,
+                                                train_loader, val_loader,
+                                                num_epochs, BS, learning_rate=LR, weight_decay=WD, patience=5,
+                                                model_name='TEST3', save=True)
