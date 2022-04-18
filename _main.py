03d078fe8b6f98f8b39c8ac7503f6942e15e0aed
RobbeDeMuynck <robbe_demuynck@yahoo.com>
2022-04-18 15:08:59 +0200

    log_folder param added

--- _main.py
+++ _main.py
@@ -8,11 +8,11 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 torch.cuda.empty_cache()
 
 ### Declare training hyperparameters ###
-num_epochs = 100 # 300
+num_epochs = 300 # 300
 batch_size = [4] # [4, 8, 12]
 learning_rate = [1e-3] # [1e-2, 5e-3, 1e-3, 1e-4, 1e-5]
 weight_decay = [0] # [0, 1e-2, 1e-4]
-patience = 100 # 5
+patience = 5 # 5
 
 ### Declare network architecture ###
 layers = [3] # [3, 4]
@@ -32,9 +32,11 @@ for LYRS in layers:
                                                 val_loader = DataLoader(MiceDataset(val_input, val_target), batch_size=BS, shuffle=True, drop_last=True)
                                                 ### Train model ###
                                                 model_name = f'LYRS={LYRS};FT={FT};BS={BS};LR={LR};WD={WD}'
+                                                log_folder = 'runlogs'
                                                 if repeats > 1:
                                                         model_name += f';RUN={i}'
+                                                        log_folder = 'runlogs_repeat'
                                                 train(LYRS, FT, device,
                                                         train_loader, val_loader,
                                                         num_epochs, BS, learning_rate=LR, weight_decay=WD, patience=patience,
-                                                        model_name=model_name, save=True)
+                                                        model_name=model_name, log_folder=log_folder, save=True)
