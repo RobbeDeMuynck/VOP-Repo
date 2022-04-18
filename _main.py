6d50409a5b2abbc86ca4251b3007ed32d0263b8c
RobbeDeMuynck <robbe_demuynck@yahoo.com>
2022-04-18 02:33:55 +0200

    overfitting loop prepared

--- _main.py
+++ _main.py
@@ -8,17 +8,17 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 torch.cuda.empty_cache()
 
 ### Declare training hyperparameters ###
-num_epochs = 300
-batch_size = [12] # [4, 8, 12]
+num_epochs = 100 # 300
+batch_size = [4] # [4, 8, 12]
 learning_rate = [1e-3] # [1e-2, 5e-3, 1e-3, 1e-4, 1e-5]
 weight_decay = [0] # [0, 1e-2, 1e-4]
-patience = 5
+patience = 100 # 5
 
 ### Declare network architecture ###
-layers = [4] # [3, 4]
-features = [4] # [4, 8, 12, 16]
+layers = [3] # [3, 4]
+features = [12] # [4, 8, 12, 16]
 
-repeats = 10
+repeats = 1
 ### Train model ###
 for LYRS in layers:
         for FT in features:
@@ -31,7 +31,9 @@ for LYRS in layers:
                                                 train_loader = DataLoader(MiceDataset(input, target), batch_size=BS, shuffle=True, drop_last=True)
                                                 val_loader = DataLoader(MiceDataset(val_input, val_target), batch_size=BS, shuffle=True, drop_last=True)
                                                 ### Train model ###
-                                                model_name = f'LYRS={LYRS};FT={FT};BS={BS};LR={LR};WD={WD}'+f';RUN={i}'
+                                                model_name = f'LYRS={LYRS};FT={FT};BS={BS};LR={LR};WD={WD}'
+                                                if repeats > 1:
+                                                        model_name += f';RUN={i}'
                                                 train(LYRS, FT, device,
                                                         train_loader, val_loader,
                                                         num_epochs, BS, learning_rate=LR, weight_decay=WD, patience=5,
