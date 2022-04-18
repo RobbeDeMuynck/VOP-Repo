83a3e590dc7361c5a9741348e793881737c842c3
RobbeDeMuynck <robbe_demuynck@yahoo.com>
2022-04-19 00:06:35 +0200

    overfitting

--- _plot_overfitting.py
+++ _plot_overfitting.py
@@ -27,18 +27,18 @@ n = len(losses)
 palette = sns.color_palette("mako_r", n)
 palette = sns.color_palette("crest", n)
 palette = sns.color_palette("colorblind")
-fs = 20
-fig, ax = plt.subplots(1, 1)
+fs = 15
+fig, ax = plt.subplots(1, 1, figsize=(10, 6))
 ax.semilogy(losses[0], lw=5, color=palette[0], label='Training losses') # semilogy
 ax.semilogy(losses[1], lw=5, color=palette[1], label='Validation losses')
 ax.axvline(x=np.argmin(losses[1]), c='k', ls='--', label='Last saved version')
 ax.set_title('MSE loss decay per epoch', fontsize=fs+10)
-ax.set_xlabel('Epoch number', fontsize=fs)
-ax.set_ylabel('MSE loss (batch-average)', fontsize=fs)
+ax.set_xlabel('Epoch number', fontsize=fs+5)
+ax.set_ylabel('MSE loss (batch-average)', fontsize=fs+5)
 ax.legend(fontsize=fs)
 ax.grid()
 
 plt.tight_layout()
-plt.savefig('Overfit_prevention.png', dpi=200)
+plt.savefig('IMAGES/Overfit_prevention.png', dpi=200)
 plt.show()
 
