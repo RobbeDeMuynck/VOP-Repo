b510981b781bde4673a6ac19204894f105df3239
RobbeDeMuynck <robbe_demuynck@yahoo.com>
2022-04-19 20:28:40 +0200

    is it learning?

---
+++ learning_main.py
@@ -0,0 +1,42 @@
+from _load_data import MiceDataset, get_data
+import torch
+from torch.utils.data import DataLoader
+from learning_train import train
+
+### Declare device ###
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+torch.cuda.empty_cache()
+
+### Declare training hyperparameters ###
+num_epochs = 300 # 300
+batch_size = [4] # [4, 8, 12]
+learning_rate = [1e-3] # [1e-2, 5e-3, 1e-3, 1e-4, 1e-5]
+weight_decay = [0] # [0, 1e-2, 1e-4]
+patience = 5 # 5
+
+### Declare network architecture ###
+layers = [3] # [3, 4]
+features = [12] # [4, 8, 12, 16]
+
+repeats = 1
+### Train model ###
+for LYRS in layers:
+        for FT in features:
+                for BS in batch_size:
+                        for LR in learning_rate:
+                                for WD in weight_decay:
+                                        for i in range(repeats):
+                                                ### Declare training & validation datasets ###
+                                                input, target, val_input, val_target = get_data(plane='transversal', val_mouse=0)
+                                                train_loader = DataLoader(MiceDataset(input, target), batch_size=BS, shuffle=True, drop_last=True)
+                                                val_loader = DataLoader(MiceDataset(val_input, val_target), batch_size=BS, shuffle=True, drop_last=True)
+                                                ### Train model ###
+                                                model_name = 'learning'
+                                                log_folder = 'runlogs_learning'
+                                                if repeats > 1:
+                                                        model_name += f';RUN={i}'
+                                                        log_folder = 'runlogs_repeat'
+                                                train(LYRS, FT, device,
+                                                        train_loader, val_loader,
+                                                        num_epochs, BS, learning_rate=LR, weight_decay=WD, patience=patience,
+                                                        model_name=model_name, log_folder=log_folder, save=True)
---
+++ learning_train.py
@@ -0,0 +1,134 @@
+from _UNET import UNet
+import torch
+import torch.nn as nn
+import torchvision
+from torch.optim import Adam
+import numpy as np
+from tqdm import tqdm
+from torch.autograd import Variable
+import time
+import json
+
+from _load_data import MiceDataset, get_data
+from torch.utils.data import DataLoader
+
+################################## TRAINING  ##################################
+def train(layers, features, device,
+        train_loader, val_loader,
+        num_epochs, batch_size, learning_rate=1e-3, weight_decay=0, patience=5,
+        model_name='TEST', log_folder='runlogs', save=True):
+
+    ### Declare network architecture ###
+    model = UNet(layers=layers, ft=features).to(device)
+    ### Declare loss function & optimizer ###
+    loss_function = nn.MSELoss().to(device)
+    optimizer = Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+
+    print('Starting with training...')
+    loss_stats = {
+        'train': [],
+        'val': [],
+        'input_target': [],
+        'pred_target': []
+        }
+    best_loss = np.inf
+    starttime = time.time()
+    for epoch in range(1, num_epochs+1):
+        model.train().to(device)
+        train_epoch_loss = 0
+        print(f"Epoch: {epoch}/{num_epochs}")
+        for input_batch, target_batch in tqdm(train_loader):
+            
+            # Put batch on GPU
+            input_batch = input_batch.to(device)
+            target_batch = target_batch.to(device)
+            optimizer.zero_grad()
+            
+            prediction_batch = model(input_batch)[0].to(device)
+            _, _, H, W = prediction_batch.shape
+            target_batch = torchvision.transforms.CenterCrop([H,W])(target_batch)
+            input_batch = torchvision.transforms.CenterCrop([H,W])(input_batch)
+            mse = loss_function(input_batch, target_batch)
+            loss = loss_function(prediction_batch, target_batch) # Compare prediction with target
+            loss.backward()
+            optimizer.step()
+
+            train_epoch_loss += loss.item()
+
+            loss_stats["pred_target"].append(loss.item())
+            loss_stats["input_target"].append(mse.item())
+            
+            
+        
+        ### Prevent overfitting ###
+        with torch.no_grad():
+            val_epoch_loss = 0
+                
+            model.eval().to(device)
+            for val_input_batch, val_target_batch in val_loader:
+                val_input_batch = val_input_batch.to(device)
+                val_target_batch = val_target_batch.to(device)
+
+                val_pred = model(val_input_batch)[0].to(device)
+                val_target_batch = torchvision.transforms.CenterCrop([H,W])(val_target_batch)
+
+                val_loss = loss_function(val_pred, val_target_batch)
+                val_epoch_loss += val_loss.item()
+        
+        # Store the batch-average MSE loss per epoch
+        loss_stats["train"].append(train_epoch_loss/len(train_loader))
+        loss_stats["val"].append(val_epoch_loss/len(val_loader))
+        
+        if loss_stats["val"][-1] < best_loss:
+            endtime = time.time()
+            best_loss, epoch_no = loss_stats["val"][-1], epoch
+            if save == True:
+                torch.save(model.state_dict(), 'MODELS/'+model_name+'.pth')
+                print(f"    --> Model saved at epoch no. {epoch_no}")
+        
+        print(f"""
+    Training loss: {loss_stats["train"][-1]}
+    Last validation losses: {loss_stats['val'][-patience:]}
+    Best loss: {best_loss}""")
+
+        if np.all(np.array(loss_stats['val'][-patience:]) > best_loss):
+            print(f"""
+    Training terminated after epoch no. {epoch}
+    Model saved as '{model_name}.pth': version at epoch no. {epoch_no}""")
+
+            break
+        elif epoch == num_epochs:
+            epoch_no = epoch
+            print(f"Model trained succesfully and saved as: '{model_name}.pth'")
+
+    ### Dictionary with model details ###
+    run = {
+        'train_time' : endtime-starttime,
+        'layers': layers,
+        'features': features,
+
+        'num_epochs': num_epochs,
+        'batch_size': batch_size,
+        'learning_rate': learning_rate,
+        'weight_decay': weight_decay,
+        'patience': patience,
+        
+        'train_loss': loss_stats["train"],
+        'val_loss': loss_stats["val"],
+        'pred_target': loss_stats["pred_target"],
+        'input_target': loss_stats["input_target"],
+
+        'num_epoch_convergence': epoch_no
+    }
+    with open(log_folder+f'/{model_name}.json', 'w+') as file:
+                    json.dump(run, file, indent=4)
+    return run
+
+##################################### Plotting MODEL losses  ##################################
+# plt.semilogy(loss_stats["train"], label='Training losses')
+# plt.semilogy(loss_stats["val"], label='Validation losses')
+# plt.ylabel('MSE loss')
+# plt.xlabel('Epoch')
+# plt.legend()
+# plt.grid()
+# plt.show()
