77617c65c0948f1d03476bbef936665025646b55
Wouter Bogaert <bogaertwouter@yahoo.com>
2022-05-11 15:32:18 +0200

    alignemnt segmentation mask

--- _predict_segment.py
+++ _predict_segment.py
@@ -1,14 +1,17 @@
+from sklearn.preprocessing import OrdinalEncoder
+from torch import slice_scatter
 from Segmentationnnn import *
 import matplotlib.pyplot as plt
 import numpy as np
 import json
 from torchsummary import summary
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # import nibabel as nib
 # import pathlib
 
 ############################# LOADING THE MODEL  #############################
-model_path = "MODELS/SEGG64.pth"
+model_path = "MODELS/SEGG_layers4_lr0.001_wd0.01_ft16.pth"
 # model_path = "MODELS\BS=8;LR=0.001;WD=0.09;FT=4.pth"
 # model_runlog = "runlogs\LYRS=3;FT=12;BS=4;LR=0.005;WD=0.json"
 
@@ -17,16 +20,18 @@ model_path = "MODELS/SEGG64.pth"
 #     layers, features = run["layers"], run["features"]
 #device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 device = torch.device('cpu')
-torch.cuda.empty_cache()
-model = UNet(4, 64).to(device)
+#torch.cuda.empty_cache()
+model = UNet(4, 16).to(device)
 model.load_state_dict(torch.load(model_path))
-#summary(model, (1, 154, 121))
+
+H,W = 144, 112
 
 ### LOAD IMAGES & NORMALIZE DATA ###
 def normalize(arr):
     return (arr-np.mean(arr))/np.std(arr)
-input, target, val_input, val_target = get_data()
-ind = len(val_input)//2
+
+input, target, val_input, val_target = get_data(val_mouse=5)
+ind = 150
 slice_input, slice_target = normalize(val_input[ind]), normalize(val_target[ind])
 print('gelukt')
 
@@ -36,25 +41,38 @@ print(f'modelinputsliceshape: {slice_to_predict.shape}')
 ### APPLY MODEL ###
 model.eval()
 x = model(slice_to_predict)[0]
+slice_to_predict = torchvision.transforms.CenterCrop([H,W])(slice_to_predict)
+slice_to_predict = torch.squeeze(slice_to_predict).detach().numpy()
+#slice_to_predict = slice_to_predict[0:144,0:112]
+print(f'imageshape:{slice_to_predict.shape}')
+
 #print(x)
 
 print(x.shape)
 
 x = torch.squeeze(x).detach().numpy()
-print(f'numpyshape:{x.shape}')
+print(f'maskshape:{x.shape}')
 
 slice_prediction =np.argmax(x,axis=0)
 
 # plot input vs prediction
-fig, axs = plt.subplots(1, 2)
-axs[0].imshow(slice_input, cmap='viridis')
-axs[0].imshow(slice_target, cmap='GnBu_r',alpha=.5)
-axs[1].imshow(slice_input,cmap='viridis')
-axs[1].imshow(slice_prediction, cmap='GnBu_r',alpha=.5)
+fig, axs = plt.subplots(1, 1)
 
 
-axs[0].set_title('Input')
-axs[1].set_title('Prediction')
+# axs[0].imshow(slice_input, cmap='bone')
+# axs[0].imshow(slice_target, cmap='viridis',alpha=.8)
+axs.imshow(slice_to_predict,cmap='bone')
+
+seg = axs.imshow(slice_prediction, cmap='viridis',alpha=.8)
+divider = make_axes_locatable(axs[1])
+cax = divider.append_axes('right')
+
+fig.colorbar(seg, cax = cax)
+axs.add
+
+
+#axs[0].set_title('Input')
+axs.set_title('Prediction')
 plt.tight_layout()
 plt.savefig(f'IMAGES/PRED_SAG.png', dpi=200)
 plt.show()
