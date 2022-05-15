f54fe75e2785b32329300d0a24ffe0769d8892ab
RobbeDeMuynck <robbe_demuynck@yahoo.com>
2022-05-15 17:04:57 +0200

    segmentation load data

--- load_data_segmentation.py
+++ load_data_segmentation.py
@@ -3,6 +3,7 @@ import torch
 from torch.utils.data import Dataset
 import pathlib
 import nibabel as nib
+from tqdm import tqdm
 
 import matplotlib.pyplot as plt
 
@@ -36,8 +37,18 @@ class MiceDataset(Dataset):
         return input, target
 
 ################################## GET PROCESSED DATA ##################################
-def get_data(plane='sagittal', val_mice=[], test_mice=[]):
+def mapping(old_organ, dict):
+    # old_organ: 3D
+    I, J, K = old_organ.shape
+    new_organ = old_organ.copy()
+    for i in range(I):
+        for j in range(J):
+            for k in range(K):
+                new_organ[i, j, k] = dict[old_organ[i, j, k]]
+    return new_organ
 
+def get_data(plane='sagittal', val_mice=[], test_mice=[]):
+    # Construct data lists
     train_input, train_target = [], []
     val_input, val_target = [], []
     test_input, test_target = [], []
@@ -45,7 +56,7 @@ def get_data(plane='sagittal', val_mice=[], test_mice=[]):
     mice = ["M01", "M02","M03", "M04", "M05", "M06", "M07","M08","M09", "M10", "M11", "M12","M13", "M14", "M15", "M16", "M17","M18","M19", "M20"]
     path = pathlib.Path(__file__).parent
 
-    # 
+    # Standardized index of organ segmentation: dictionaries
     ClassNames = {
         0: 'unclassified',
         1: 'Heart',
@@ -61,8 +72,10 @@ def get_data(plane='sagittal', val_mice=[], test_mice=[]):
         11: 'Trachea',
         12: 'Tumor'
     }
+    name_to_idx_standard = {val: key for key, val in ClassNames.items()}
 
-    for i, mouse in enumerate(mice):
+    # Add each mouse
+    for i, mouse in tqdm(enumerate(mice)):
         # Declare path to data of each mouse
         timestamp = "024h"
 
@@ -74,30 +87,53 @@ def get_data(plane='sagittal', val_mice=[], test_mice=[]):
         if not path_class.is_file():
             path_class = path / f"data_segmentation/{mouse}_{timestamp}/Organ1.cls"
         
+        # Read-in data
         ct = nib.load(path_ct).get_fdata()
         organ = nib.load(path_organ).get_fdata()
-        L, H, W = ct.shape
-        L_, H_, W_ = organ.shape
-        print(f'shape of ct mouse {mouse}:\t({L}, {H}, {W})')
-        print(f'shape of organ mouse {mouse}:\t({L_}, {H_}, {W_})\n')
-        if plane == 'sagittal':
-            ct, organ = ct, organ
-            print(f'shape of ct mouse {mouse}:\t({ct.shape})')
-        for j in range(100):
-            if i+1 in val_mice:
-                val_input.append(ct)
-                val_target.append(organ)
-            if i+1 in test_mice:
-                test_input.append(ct)
-                test_target.append(organ)
-            if i+1 not in val_mice+test_mice:
-                train_input.append(ct)
-                train_target.append(organ)
-        if i == 0:
-            plt.imshow(ct[50,:,:], cmap='bone')
-            plt.imshow(organ[50,:,:], cmap='viridis',alpha=.5)
-            plt.show()
 
+        # L, H, W = ct.shape
+        # L_, H_, W_ = organ.shape
+        # print(f'shape of ct mouse {mouse}:\t({L}, {H}, {W})')
+        # print(f'shape of organ mouse {mouse}:\t({L_}, {H_}, {W_})')
+
+        # Crop all scans to same dimensions
+        ct, organ = ct[:140, :102, :190], organ[:140, :102, :190]
+
+        # if plane == 'sagittal':
+        #     ct, organ = ct[:140, :102, :194], organ[:140, :102, :194]
+
+        # Standardized index of organ segmentation
+        # with open(path_class) as f:
+        #     content = f.read()
+        #     indices = content.split('\n')[1].split('=')[1].split('|')
+        #     names = content.split('\n')[2].split('=')[1].split('|')
+        #     dict = {int(idx): name for idx, name in zip(indices, names)}
+        
+        #     mapping_old_to_standard = {}
+        #     for old_idx, old_name in dict.items():
+        #         mapping_old_to_standard[old_idx] = name_to_idx_standard[old_name]
+        #     print(mapping_old_to_standard)
+        #     organ = mapping(organ, mapping_old_to_standard)
+
+        # Append each mouse to
+        if i+1 in val_mice:
+            # print(ct[0].shape)
+            val_input.extend([ct_slice for ct_slice in ct])
+            val_target.extend([organ_slice for organ_slice in organ])
+        if i+1 in test_mice:
+            test_input.extend([ct_slice for ct_slice in ct])
+            test_target.extend([organ_slice for organ_slice in organ])
+        if i+1 not in val_mice+test_mice:
+            train_input.extend([ct_slice for ct_slice in ct])
+            train_target.extend([organ_slice for organ_slice in organ])
+
+        # if i == 0:
+        #     plt.imshow(ct[50,:,:], cmap='bone')
+        #     plt.imshow(organ[50,:,:], cmap='viridis',alpha=.5)
+        #     plt.show()
+    # print(train_target)
+
+    # Convert data lists to arrays
     train_input, train_target = np.array(train_input), np.array(train_target, dtype=int)
     val_input, val_target = np.array(val_input), np.array(val_target, dtype=int)
     test_input, test_target = np.array(test_input), np.array(test_target, dtype=int)
@@ -105,3 +141,10 @@ def get_data(plane='sagittal', val_mice=[], test_mice=[]):
     return train_input, train_target, val_input, val_target, test_input, test_target
 
 train_input, train_target, val_input, val_target, test_input, test_target = get_data(plane='sagittal', val_mice=[15, 16, 17], test_mice=[18, 19, 20])
+
+# PLOT
+idx = 140+70
+ct, organ = val_input[idx], val_target[idx] 
+plt.imshow(ct, cmap='bone')
+plt.imshow(organ, cmap='Paired',alpha=.5)
+plt.show()
