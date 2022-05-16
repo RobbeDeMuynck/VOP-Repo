c2ee1a8fbd2c7c5711848858bc10567c10455a43
Thibault <thibault.sys@hotmail.com>
2022-05-16 10:13:18 +0200

    ok

--- segmentation_performance.py
+++ segmentation_performance.py
@@ -19,8 +19,9 @@ def normalize(arr):
     return (arr-np.mean(arr))/np.std(arr)
 
 ### Load the model
-model_path = "MODELS/SEGG_layers4_lr0.001_wd0.01_ft16.pth"
+# model_path = "MODELS/SEGG_layers4_lr0.001_wd0.01_ft16.pth"
 model_path = "MODELS/SEGMENT_3lyrs_12fts.pth"
+# model_path = "MODELS/LargeSeg_layers4_lr0.001_wd0_ft12.pth"
 
 # model_path = "MODELS\BS=8;LR=0.001;WD=0.09;FT=4.pth"
 # model_runlog = "runlogs\LYRS=3;FT=12;BS=4;LR=0.005;WD=0.json"
@@ -33,7 +34,7 @@ model_path = "MODELS/SEGMENT_3lyrs_12fts.pth"
 device = torch.device('cpu')
 torch.cuda.empty_cache()
 model = UNet(3, 12).to(device)
-model.load_state_dict(torch.load(model_path))
+model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
 
 ### Read-in data
 train_input, train_target, val_input, val_target, test_input, test_target = get_data(plane='sagittal', val_mice=[15, 16, 17], test_mice=[18, 19, 20], standardize=True)
